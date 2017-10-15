80e33f7d49d7064ce42ae3fd48e80c94755f3af9
liblabs-mac <liblabs-mac@MacBook-Pro-8.local>
2017-10-15 12:24:35 -0700

    session 2 updates

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -12,11 +12,17 @@ class ViewController: UIViewController {
     @IBOutlet var totalBillAmount: UILabel!
     @IBOutlet var initialBillAmount: UITextField!
     @IBOutlet var actualTipAmount: UILabel!
+	@IBOutlet var tipIndex: UISegmentedControl!
+	
+	let tipVals = [0.10, 0.15, 0.20]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+		
+        actualTipAmount.text = "Bill amount not entered"
+		totalBillAmount.text = "Bill amount not entered"
+		
         let keyboardDismiss = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
         self.view.addGestureRecognizer(keyboardDismiss)
     }
@@ -34,13 +40,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateTipValue(sender: AnyObject) {
+        print("tip percent changed")
+        let bill = Double(initialBillAmount.text!) ?? 0
+        let tip = bill * tipVals[tipIndex.selectedSegmentIndex]
+        actualTipAmount.text = String(format: "$%.2f", tip)
+        totalBillAmount.text = String(format: "$%.2f", tip + bill)
+    }
+    
+    @IBAction func billAmount(sender: AnyObject) {
+        print("Bill amount entered.")
+        let bill = Double(initialBillAmount.text!) ?? 0
+        let tip = bill * tipVals[tipIndex.selectedSegmentIndex]
+        actualTipAmount.text = String(format: "$%.2f", tip)
+        totalBillAmount.text = String(format: "$%.2f", tip + bill)
+    }
 
     @IBAction func helloWorld(sender: AnyObject) {
         print("Hello World! The app works.")
     }
     
-    @IBAction func billAmountEntered(sender: AnyObject) {
-        print("Bill amount entered.")
-    }
 }
 
